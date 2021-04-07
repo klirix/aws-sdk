f2960f355844610fb9d2af433e69d739c42813a3
Askhat Saiapov <askhat.saiapov@gmail.com>
2021-04-07 23:56:33 +0300

    ensure it genereates good xml

--- src/protocols/rest_xml.cr
+++ src/protocols/rest_xml.cr
@@ -1,14 +1,16 @@
-require "./protocol.cr"
 require "xml"
 
 module AWSSdk
+  annotation Field
+  end
   module RestXML
-    include Protocol
     module Structure
-      {% begin %}
-        {% properties = {} of Nil => Nil %}
-        {% for ivar in @type.instance_vars %}
-          {% ann = ivar.annotation(RestXML::Protocol) %}
+
+      def serialize(xml : XML::Builder)
+        {% begin %}
+          {% properties = {} of Nil => Nil %}
+          {% for ivar in @type.instance_vars %}
+            {% ann = ivar.annotation(AWSSdk::Field) %}
             {%
               properties[ivar.id] = {
                 type:         ivar.type,
@@ -23,63 +25,59 @@ module AWSSdk
               }
             %}
           {% end %}
-      def serialize(xml : XML::Builder)
-        {% for name, props in properties %}
-          {% if props[:location] == :body %}
-            {% if props[:structure] %}
-              xml.element({{props[:name]}}) {
-                {{name.id}}.serialize(xml)
-              }
-            {% elsif props[:type] <= Array %}
-              {{name.id}}.each do |el|
+          {% for name, props in properties %}
+          {% puts props %}
+            {% if props[:location] == :body %}
+              {% if props[:is_structure] %}
                 xml.element({{props[:name]}}) {
-                  {% if [String, Float32, Int32].includes? props[:type].type_vars[0]  %}
-                    xml.text el
-                  {% else %}
-                    xml.text el
-                  {% end %}
+                  {{name.id}}.serialize(xml)
                 }
-              end
-            {% else %}
-              xml.element({{props[:name]}}) { xml.text {{name.id}}}
+              {% elsif props[:type] <= Array %}
+                {{name.id}}.each do |el|
+                  xml.element({{props[:name]}}) {
+                    {% if [String, Float32, Int32].includes? props[:type].type_vars[0]  %}
+                      xml.text el
+                    {% else %}
+                      el.serialize(xml)
+                    {% end %}
+                  }
+                end
+              {% else %}
+                xml.element({{props[:name]}}) { xml.text {{name.id}}}
+              {% end %}
             {% end %}
           {% end %}
         {% end %}
       end
-      def self.deserialize(xml : XML::Node)
-        {% for name, props in properties %}
-          {% if props[:location] == :body %}
-            {% if props[:structure] %}
-              @{{name}} = {{props[:type]}}.deserialize(
-                xml.xpath_node({{props[:name]}})
-              )
-            {% elsif props[:type] <= Array %}
-              {{name.id}} = xml.xpath_nodeset({{props[:name]}}).map do |node|
-                {% if props[:type].type_vars[0] === String %}
-                  node.content
-                {% elsif props[:type].type_vars[0] === Float32 %}
-                  node.content.to_f32
-                {% elsif props[:type].type_vars[0] === Float64 %}
-                  node.content.to_f64
-                {% else %}
-                  {{props[:type]}}.deserialize(xml)
-                {% end %}
-              end
-            {% else %}
-              xml.element({{props[:name]}}) { xml.text {{name.id}}}
-            {% end %}
+      def process(request : HTTP::Request)
+        {% begin %}
+          {% properties = {} of Nil => Nil %}
+          {% for ivar in @type.instance_vars %}
+            {% ann = ivar.annotation(AWSSdk::Field) %}
+            {%
+              properties[ivar.id] = {
+                type:         ivar.type,
+                key:          ((ann && ann[:key]) || ivar).id.stringify,
+                has_default:  ivar.has_default_value?,
+                default:      ivar.default_value,
+                nilable:      ivar.type.nilable?,
+                location:     ann && ann[:location],
+                name:         ann && ann[:name],
+                is_structure: ann && ann[:structure] || false,
+
+              }
+            %}
           {% end %}
-        {% end %}
-      end
-      macro included
-        def process(request : HTTP::Request)
           {% for name, props in properties %}
             {% if props[:location] == :query %}
-              request.query_params[{{props[:name]}}] = @{{name.id}}
+              if q_param = @{{name.id}}
+                request.query_params[{{props[:name]}}] = q_param.to_s
+              end
             {% elsif props[:location] == :body_io %}
-              {% if props[:structure] == true %}
-                request.body = XML.build do |x|
-                  x.element({{props[:name]}}) do
+              {% puts props %}
+              {% if props[:is_structure] == true %}
+                request.body = XML.build do |xml|
+                  xml.element({{props[:name]}}) do
                     {{name.id}}.serialize(xml)
                   end
                 end
@@ -88,17 +86,83 @@ module AWSSdk
                 request.headers["Content-Length"] = "#{{{name.id}}.size}"
               {% end %}
             {% elsif props[:location] == :header %}
-              request.headers[{{props[:name]}}] = @{{name.id}}
+              if h_param = @{{name.id}}
+                request.headers[{{props[:name]}}] = h_param.to_s
+              end
             {% end %}
           {% end %}
           request
+        {% end %}
+      end
+      macro included
+        def self.deserialize(xml : XML::Node)
+          {% begin %}
+            {% properties = {} of Nil => Nil %}
+            {% for ivar in @type.instance_vars %}
+              {% ann = ivar.annotation(AWSSdk::Field) %}
+              {%
+                properties[ivar.id] = {
+                  type:         ivar.type,
+                  key:          ((ann && ann[:key]) || ivar).id.stringify,
+                  has_default:  ivar.has_default_value?,
+                  default:      ivar.default_value,
+                  nilable:      ivar.type.nilable?,
+                  location:     ann && ann[:location],
+                  name:         ann && ann[:name],
+                  is_structure: ann && ann[:structure] || false,
+
+                }
+              %}
+            {% end %}
+            {% for name, props in properties %}
+              {% if props[:location] == :body %}
+                {% if props[:is_structure] %}
+                  @{{name}} = {{props[:type]}}.deserialize(
+                    xml.xpath_node({{props[:name]}})
+                  )
+                {% elsif props[:type] <= Array %}
+                  {{name.id}} = xml.xpath_nodeset({{props[:name]}}).map do |node|
+                    {% if props[:type].type_vars[0] === String %}
+                      node.content
+                    {% elsif props[:type].type_vars[0] === Float32 %}
+                      node.content.to_f32
+                    {% elsif props[:type].type_vars[0] === Float64 %}
+                      node.content.to_f64
+                    {% else %}
+                      {{props[:type]}}.deserialize(xml)
+                    {% end %}
+                  end
+                {% else %}
+                  xml.element({{props[:name]}}) { xml.text {{name.id}}}
+                {% end %}
+              {% end %}
+            {% end %}
+          {% end %}
         end
         def self.from_response(request : HTTP::Client::Response)
+          {% begin %}
+          {% properties = {} of Nil => Nil %}
+          {% for ivar in @type.instance_vars %}
+            {% ann = ivar.annotation(RestXML::Protocol) %}
+            {%
+              properties[ivar.id] = {
+                type:         ivar.type,
+                key:          ((ann && ann[:key]) || ivar).id.stringify,
+                has_default:  ivar.has_default_value?,
+                default:      ivar.default_value,
+                nilable:      ivar.type.nilable?,
+                location:     ann && ann[:location],
+                name:         ann && ann[:name],
+                is_structure: ann && ann[:structure] || false,
+
+              }
+            %}
+          {% end %}
           node = XML.parse(request.body)
           new(
             {% for name, props in properties %}
             {% if props[:location] == :body_io %}
-              {% if props[:structure] == true %}
+              {% if props[:is_structure] == true %}
                 {{name.id}}: {{props[:type]}}.deserialize(
                   node.xpath_node({{props[:name]}}).not_nil!
                 ),
@@ -112,10 +176,9 @@ module AWSSdk
             {% end %}
           {% end %}
           )
-
+          {% end %}
         end
       end
-      {% end %}
     end
   end
 end
